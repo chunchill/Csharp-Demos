d7df829acbb847dcdf0a290e8886d7df0f34b91a
chunchill <chunchill@163.com>
2013-12-20 00:07:53 +0800

    TaskFactory and SychronizationContext

---
+++ SynchronizationContextTaskScheduler/GUI.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SynchronizationContextTaskScheduler
+{
+    public partial class GUI : Form
+    {
+        public GUI()
+        {
+            InitializeComponent();
+            Text = "Synchronization Context Task Scheduler Demo";
+            m_SyncContexTaskScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+            Visible = true;
+            Width = 400;
+            Height = 100;
+        }
+
+        private readonly TaskScheduler m_SyncContexTaskScheduler;
+        private CancellationTokenSource m_cts;
+
+        protected override void OnMouseClick(MouseEventArgs e)
+        {
+            if (m_cts != null)
+            {
+                m_cts.Cancel();
+                m_cts = null;
+            }
+            else
+            {
+                Text = "Operation running";
+            }
+            m_cts = new CancellationTokenSource();
+
+            var t = new Task<Int32>(() => Sum(m_cts.Token, Int32.MaxValue), m_cts.Token);
+            t.Start();
+            t.ContinueWith(task => { Text = "Result" + task.Result; }, CancellationToken.None, TaskContinuationOptions.OnlyOnRanToCompletion, m_SyncContexTaskScheduler);
+            t.ContinueWith(task => { Text = "Operation Cancled"; }, CancellationToken.None, TaskContinuationOptions.OnlyOnCanceled, m_SyncContexTaskScheduler);
+            t.ContinueWith(task => { Text = "Operation Faulted"; }, CancellationToken.None, TaskContinuationOptions.OnlyOnFaulted, m_SyncContexTaskScheduler);
+            base.OnMouseClick(e);
+        }
+
+        private static Int32 Sum(CancellationToken ct, Int32 n)
+        {
+            Int32 sum = 0;
+            for (; n > 0; n--)
+            {
+                ct.ThrowIfCancellationRequested();
+                checked { sum += n; }
+            }
+            return sum;
+        }
+
+
+    }
+}
---
+++ TaskFactory/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelDemos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TaskFactoryDemo();
+            Console.Read();
+        }
+
+        private static Int32 Sum(CancellationToken ct, Int32 n)
+        {
+            Int32 sum = 0;
+            for (; n > 0; n--)
+            {
+                ct.ThrowIfCancellationRequested();
+                checked { sum += n; }
+            }
+            return sum;
+        }
+
+        private static void TaskFactoryDemo()
+        {
+            Task parent = new Task(() =>
+            {
+                var cts = new CancellationTokenSource();
+                var tf = new TaskFactory<Int32>(cts.Token,
+                    TaskCreationOptions.AttachedToParent,
+                    TaskContinuationOptions.ExecuteSynchronously,
+                    TaskScheduler.Default);
+                var childTasks = new[] { 
+                    tf.StartNew(()=>Sum(cts.Token,10000)),
+                    tf.StartNew(()=>Sum(cts.Token,20000)),
+                    tf.StartNew(()=>Sum(cts.Token,Int32.MaxValue))
+                };
+                for (Int32 task = 0; task < childTasks.Length; task++)
+                {
+                    childTasks[task].ContinueWith(t => cts.Cancel(), TaskContinuationOptions.OnlyOnFaulted);
+                }
+
+                // CancellationToken.None would override the one passed into TaskFactory,so even the cts.Token is canceled,this task would be always excuted.
+                tf.ContinueWhenAll(childTasks, 
+                    complatedTasks => complatedTasks.Where(t => !t.IsFaulted && !t.IsCanceled).Max(t => t.Result), CancellationToken.None).
+                    ContinueWith(t => Console.WriteLine("The maximum is: " + t.Result), TaskContinuationOptions.ExecuteSynchronously);
+              
+            });
+
+            parent.ContinueWith(p =>
+            {
+                StringBuilder sb = new StringBuilder("The following exeption(s) occured: " + Environment.NewLine);
+                foreach(var e in p.Exception.Flatten().InnerExceptions){
+                    sb.AppendLine(""+e.GetType().ToString());
+                    Console.WriteLine(sb.ToString());
+                }
+            },TaskContinuationOptions.OnlyOnFaulted);
+
+            parent.Start();
+        }
+    }
+}
